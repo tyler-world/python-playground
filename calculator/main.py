8626dbd526704b380cbc58a8cab206e7fba3898d
tyler <n.tyler975@gmail.com>
2025-11-11 12:20:56 -0800

    Convert calculator from submodule to normal folder

---
+++ calculator/main.py
@@ -0,0 +1,31 @@
+from functions import add, subtract, multiply, divide
+
+print("Select your choice:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiply")
+print("4. Divide\n")
+
+choice = int(input())
+
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
+
+if choice == 1:
+    result = add(num1, num2)
+elif choice == 2:
+    result = subtract(num1, num2)
+elif choice == 3:
+    result = multiply(num1, num2)
+elif choice == 4:
+    result = divide(num1, num2)
+else:
+    result = "Invalid input."
+
+if isinstance(result, str):
+    print(result) # it's an error message
+else:
+    if result == int(result):
+        print("Result: ", int(result)) # whole number, print as int
+    else:
+        print(f"Result: {result:.2f}") # decimal number, print with 2 decimal places
